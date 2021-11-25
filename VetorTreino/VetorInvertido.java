241707e93c25b32a1bf72a3adde0bcebd2b4369a
Moisés Vicente Bezerra de Souza <93298283+MoisesSouza16Tech@users.noreply.github.com>
2021-11-25 10:50:35 -0300

    Update VetorInvertido.java

--- VetorTreino/VetorInvertido.java
+++ VetorTreino/VetorInvertido.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 public class VetorInvertido {
-	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		final int TAM=10;
-		int a[], b[], i;
-		a = new int[TAM];
-		b = new int[TAM];
-		
-		for(i=0; i<TAM; i++) {
-			System.out.println("Digite o"+(i+1)+"valor do vetor A");
-			a[i] = in.nextInt();	
-			b[i] = a[i] * a[i];
-		}
-		
-		System.out.print("\nB = ");
-		for(i=0; i<TAM; i++) {
-			System.out.print(b[i]+"  ");
-			
-		}
-	}
+		int i, j, a[], b[];
+		a = new int[5];
+		b = new int[5];
 
+		j = 4;
+		for (i = 0; i < 5; i++) {
+			System.out.println("Digite o " + (i + 1) + " elemento do vetor");
+			a[i] = in.nextInt();
+			b[j] = a[i];
+			j--;
+
+		}
+		System.out.print("\nVetor A [ ");
+		for (i = 0; i < 5; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println(" ] ");
+
+		System.out.print("\nVetor B [ ");
+		for (i = 0; i < 5; i++) {
+			System.out.print(b[i] + " ");
+		}
+		System.out.print(" ] ");
+	}
 }
